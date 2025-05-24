bab507a078420e38711dc3d0dff234de294e2f37
FishieCat <correctfishcat@gmail.com>
2025-05-24 21:41:44 +0200

    more features, file ending

--- itch-scr-grab.user.js
+++ itch-scr-grab.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         itch.io screenshots grab-buddy
 // @namespace    http://tampermonkey.net/
-// @version      2025-04-06
-// @description  grab screens from itch.io a bit quicker
+// @version      2025-05-24
+// @description  grab screens from itch.io a bit quicker, makes checking file upload dates and publish/update dates a bit easier
 // @match        https://*.itch.io/*
 // @icon         https://itch.io/favicon.ico
 // @require      https://git.io/vMmuf
@@ -13,6 +13,8 @@
     'use strict';
 
     // Your code here...
+
+    const itch_api_key = '[ENTER YOUR FREE ITCH IO API KEY HERE SEE https://github.com/FishieCat/itch.io-api-upload-check ]';
 
     function log(str) {
         console.log('%c ' + str + ' ', 'background: white; color: grey');
@@ -80,7 +82,76 @@
             });
         });
 
-    } // function maal() end
+        // add uploads API link
+
+        const scripts = document.querySelectorAll('script:not(.isc)');
+        for (let script of scripts) {
+            $(script).addClass('isc');
+            if (script.textContent.includes('game_id')) {
+                const match = script.textContent.match(/game_id["']?\s*:\s*(\d+)/);
+                if (match) {
+                    const gameId = match[1];
+                    console.log('Extracted game_id:', gameId);
+
+                    // Create the URL using gameId and itch_api_key
+                    const url = `https://itch.io/api/1/${itch_api_key}/game/${gameId}/uploads`;
+
+                    // Create the <p> element with the link
+                    const $p = $('<p id="isc_uploads" style="text-align:center;"></p>').append(
+                        $('<a></a>').attr('href', url).text(url)
+                    );
+
+                    // Prepend to #wrapper
+                    $('#wrapper').prepend($p);
+
+                    break;
+                }
+            }
+        }
+
+        // add yyyy-mm-dd dates to infobox
+        $('.game_info_panel_widget abbr[title]:not(.isc)').each(function() {
+            const $abbr = $(this).addClass('isc');
+            $('.info_panel_wrapper').attr('style', 'display: block !important;');
+            const title = $abbr.attr('title'); // e.g. "30 June 2024 @ 04:24 UTC"
+
+            // Extract day, month name, year
+            const m = title.match(/(\d{1,2}) (\w+) (\d{4})/);
+            if (!m) return;
+
+            const [ , day, monthName, year ] = m;
+            const monthMap = {
+                January:'01', February:'02', March:'03', April:'04',
+                May:'05', June:'06', July:'07', August:'08',
+                September:'09', October:'10', November:'11', December:'12'
+            };
+            const mm = monthMap[monthName];
+            if (!mm) return;
+
+            const dd = day.padStart(2,'0');
+            const iso = `${year}-${mm}-${dd}`;
+
+            // only add once
+            if ($abbr.next('input.isc-date').length) return;
+
+            // create the input
+            const $input = $('<input type="text" readonly>')
+            .addClass('isc-date')
+            .css({
+                'width': '8em',
+                'margin-left': '0.5em',
+                'font-size': '0.9em'
+            })
+            .val(iso)
+            .on('click', function() {
+                this.select();
+            });
+
+            // insert after the abbr
+            $abbr.after($input);
+        });
+
+    } // function isc() end
 
     isc();
     setInterval(isc, 1000);
